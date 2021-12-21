5fe21043c59f4b331c50670c2eeebea623a0ad2a
Farhatjan <faralost@gmail.com>
2021-12-21 14:05:08 +0600

    created model Task

--- todolistapp/models.py
+++ todolistapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    new = 'new'
+    in_progress = 'in_progress'
+    done = 'done'
+    status_choices = [(new, 'Новая'), (in_progress, 'В процессе'), (done, 'Сделано')]
+
+    task = models.CharField(max_length=300, verbose_name='Задача')
+    status = models.CharField(max_length=20, choices=status_choices, default=new, verbose_name='статус')
+    deadline = models.DateField(max_length=10, null=True, blank=True, default=None, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f"{self.pk}. {self.task}: {self.status}"
+
+    class Meta:
+        db_table = 'Tasks'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
