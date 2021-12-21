4fc027d87b1e292670b94e49098627874069e5ce
Farhatjan <faralost@gmail.com>
2021-12-21 14:30:12 +0600

    registered Task model in admin panel

--- todolistapp/admin.py
+++ todolistapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from todolistapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'task', 'status', 'deadline']
+    list_filter = ['status']
+    search_fields = ['task']
+    fields = ['task', 'status', 'deadline']
+
+
+admin.site.register(Task, TaskAdmin)
