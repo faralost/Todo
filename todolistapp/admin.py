a2b38ff9f21b46d65961fcfd3fab6711b6bb1266
Farhatjan <faralost@gmail.com>
2021-12-25 17:02:30 +0600

    added task_description field to admin panel and refreshed dump.json fixture

--- todolistapp/admin.py
+++ todolistapp/admin.py
@@ -4,10 +4,10 @@ from todolistapp.models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'task', 'status', 'deadline']
+    list_display = ['id', 'task', 'status', 'deadline', 'task_description']
     list_filter = ['status']
     search_fields = ['task']
-    fields = ['task', 'status', 'deadline']
+    fields = ['task', 'status', 'deadline', 'task_description']
 
 
 admin.site.register(Task, TaskAdmin)
