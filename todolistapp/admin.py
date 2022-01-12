8b178a09db54bb9c1091fc58e3aab1a783a2a931
Farhatjan <faralost@gmail.com>
2022-01-12 17:41:29 +0600

    created type model, status model, refactored task model, added them to admin panel amd made migrations

--- todolistapp/admin.py
+++ todolistapp/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
-from todolistapp.models import Task
+from todolistapp.models import Task, Status, Type
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'task', 'status', 'deadline', 'task_description']
-    list_filter = ['status']
+    list_display = ['id', 'task', 'type', 'status']
+    list_filter = ['status', 'type', 'status']
     search_fields = ['task']
-    fields = ['task', 'status', 'deadline', 'task_description']
+    fields = ['task', 'description', 'type', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
 
 
 admin.site.register(Task, TaskAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
--- todolistapp/models.py
+++ todolistapp/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 
 
-class Task(models.Model):
-    new = 'new'
-    in_progress = 'in_progress'
-    done = 'done'
-    status_choices = [(new, 'Новая'), (in_progress, 'В процессе'), (done, 'Сделано')]
-
-    task = models.CharField(max_length=300, verbose_name='Задача')
-    status = models.CharField(max_length=20, choices=status_choices, default=new, verbose_name='статус')
-    deadline = models.DateField(max_length=10, null=True, blank=True, default=None, verbose_name='Дата выполнения')
-    task_description = models.TextField(max_length=3000, null=True, blank=True, default=None,
-                                        verbose_name='Подробное описание')
-
-    def __str__(self):
-        return f"{self.pk}. {self.task}: {self.status}"
+class Type(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Тип')
 
     class Meta:
-        db_table = 'Tasks'
+        db_table = 'types'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Статус')
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=300, verbose_name='Задача')
+    description = models.TextField(max_length=3000, null=True, blank=True, default=None,
+                                   verbose_name='Полное описание')
+    status = models.ForeignKey('todolistapp.Status', on_delete=models.RESTRICT, related_name='tasks',
+                               verbose_name='Статус')
+    type = models.ForeignKey('todolistapp.Type', on_delete=models.RESTRICT, related_name='tasks', verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.task}"
+
+    class Meta:
+        db_table = 'tasks'
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
