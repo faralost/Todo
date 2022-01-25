9b2146b0664971e5fbb4b625f3162501662d6df7
Farhatjan <faralost@gmail.com>
2022-01-25 20:42:58 +0600

    created form to simple search by task name or task description and refactored index view

--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,19 +1,25 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView, FormView, ListView
 
-from todolistapp.forms import TaskForm
+from todolistapp.forms import TaskForm, SimpleSearchForm
 from todolistapp.models import Task
 
 
 class IndexView(ListView):
+    model = Task
     template_name = 'index.html'
     context_object_name = 'tasks'
     paginate_by = 5
 
     def get_queryset(self):
-        return Task.objects.order_by('-created_at')
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(task__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset.order_by('-created_at')
 
     def post(self, request, *args, **kwargs):
         tasks_id = request.POST.getlist('tasks_id')
@@ -21,6 +27,26 @@ class IndexView(ListView):
             task = Task.objects.get(pk=id)
             task.delete()
         return redirect('index')
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['form'] = SimpleSearchForm(initial={"search": self.search_value})
+            context['search'] = self.search_value
+        return context
 
 
 class TaskView(TemplateView):
